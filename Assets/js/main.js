a1cd39ccdfccd022944c67da6baa423238dc183a
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-06-05 08:08:54 -0500

    feat(Facturacion Php Hunters): :construction: Funcionalidad añadir - eliminar
    Se añade funcionalidad para que se pueda añadir y eliminar productos

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,25 +1,33 @@
 let btnAdd = document.querySelector("#btn-add");
 let listProductos = document.querySelector("#list-productos");
 
-let plantilla=`<div class="row data_producto">
-<div class="form-group cod">
-    <label>Código</label>
-    <input type="number">
-</div>
-<div class="form-group prod">
-    <label>Producto</label>
-    <input type="text">
-</div>
-<div class="form-group cant">
-    <label>Cantidad</label>
-    <input type="number">
-</div>
-<div class="form-group valor_uni">
-    <label>Valor Unitario</label>
-    <input type="number">
-</div>
-</div>`;
+let plantilla=`
+    <div class="row data_producto">
+        <div class="form-group cod">
+            <label>Código</label>
+            <input type="number" name="prod_codigo[]" value="10">
+        </div>
+        <div class="form-group prod">
+            <label>Producto</label>
+            <input type="text" name="prod_nombre[]" value="Manzanas">
+        </div>
+        <div class="form-group cant">
+            <label>Cantidad</label>
+            <input type="number" name="prod_cantidad[]" value="10">
+        </div>
+        <div class="form-group valor_uni">
+            <label>Valor Unitario</label>
+            <input type="number" name="prod_valorUn[]" value="20000">
+        </div>
+        <button type="button" class="btnEliminar" onclick="eliminarProducto(this)">x</button>
+    </div> 
+`;
 
 btnAdd.addEventListener("click",()=>{
     listProductos.insertAdjacentHTML("beforeend", plantilla)    
 })
+
+function eliminarProducto(btn){
+    var productoDiv = btn.parentNode;
+    productoDiv.parentNode.removeChild(productoDiv);
+}
